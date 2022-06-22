330512f8399b1c81cf4ba9df24d4855d312fd0a8
Daniel Boadzie <boadziedaniel43@gmail.com>
2022-06-22 13:47:29 +0000

    Install vaderSentiment library for sentimental analysis
    Import the libraries
    Add a pydantic class for our input
    Add a post route that accepts a text of type string and returns a result with sentiment and confident score.

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 api = FastAPI(title="Fast_Sentiment - Sentimental Analyzer API")
+
+
+class TextInput(BaseModel):
+    text: str
 
 
 @api.get("/")
 def home():
     return {"Message": "Welcome to Fast_Sentiment!"}
+
+
+@api.post("/sentiment")
+async def get_sentiment(input: TextInput):
+    analyzer = SentimentIntensityAnalyzer()
+    result = analyzer.polarity_scores(input.text)
+    sentiment = None
+    if result["compound"] >= 0.05:
+        sentiment = "Positive"
+    elif result["compound"] <= -0.05:
+        sentiment = "Negative"
+    else:
+        sentiment = "Neutral"
+    return {
+        "result": f"The sentiment of the text is: {sentiment} and the confident score is {round(result['compound'], 2)*100}%"
+    }
 
 
 # run the API
